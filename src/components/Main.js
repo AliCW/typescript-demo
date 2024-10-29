3eb6227a730ccc4fcf670b11953dd2920b2f8ada
Ali <aliwhitrow1@gmail.com>
2024-10-29 11:00:27 +0000

    interface config added for map states

--- src/components/Main.js
+++ src/components/Main.js
@@ -14,17 +14,25 @@ exports.__esModule = true;
 var React = require("react");
 var react_1 = require("react");
 var Map_1 = require("./Map");
+var fi_1 = require("react-icons/fi");
 var react_slider_1 = require("react-slider");
-function Speed(_a) {
-    var num = _a.num;
-    return (React.createElement("h2", { className: "sub-header" }, num.toString()));
+function MapConfig(config) {
+    return (React.createElement("div", null,
+        React.createElement("h2", { className: "sub-header" }, config.map),
+        React.createElement("h2", { className: "sub-header" }, config.speed.toString())));
 }
 function Main() {
     var _a = (0, react_1.useState)(0.1), speed = _a[0], setSpeed = _a[1];
+    var _b = (0, react_1.useState)("geoOrthographic"), map = _b[0], setMap = _b[1];
     return (React.createElement("div", null,
-        React.createElement("h1", { className: "header" }, "Typescript Demo"),
-        React.createElement(react_slider_1["default"], { ariaLabel: "globe-speed-slider", min: -2, max: 2, defaultValue: 0.1, step: 0.1, renderThumb: function (props, state) { return React.createElement("div", __assign({}, props), " | "); }, onChange: function (state) { return setSpeed(state); }, thumbClassName: "speed-slider-thumb", trackClassName: "speed-slider-track", withTracks: true }),
-        React.createElement(Speed, { num: speed }),
+        React.createElement("h1", { className: "header" }, "Typescript Demonstration with React Simple Maps"),
+        React.createElement(fi_1.FiChevronsLeft, { className: "slider-arrow" }),
+        React.createElement(react_slider_1["default"], { ariaLabel: "globe-speed-slider", min: -2, max: 2, defaultValue: 0.1, step: 0.1, renderThumb: function (props, state) { return React.createElement("div", __assign({}, props),
+                " ",
+                speed,
+                " "); }, onChange: function (state) { return setSpeed(state); }, thumbClassName: "slider-thumb", withTracks: true }),
+        React.createElement(fi_1.FiChevronsRight, { className: "slider-arrow" }),
+        React.createElement(MapConfig, { map: map, speed: speed }),
         React.createElement(Map_1.Map, { speed: speed })));
 }
 exports["default"] = Main;
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,33 +1,44 @@
 import * as React from "react";
 import { useState } from "react";
 import { Map } from "./Map";
+import { FiChevronsLeft, FiChevronsRight } from "react-icons/fi";
 import ReactSlider from "react-slider";
 
-function Speed({ num }: { num: Number }) {
+interface MapValue {
+    map: String;
+    speed: Number;
+}
+
+function MapConfig( config: MapValue ) {
     return (
-        <h2 className="sub-header">{num.toString()}</h2>
+        <div>
+            <h2 className="sub-header">{config.map}</h2>
+            <h2 className="sub-header">{config.speed.toString()}</h2>
+        </div>
     );
 }
 
-
 export default function Main(){
     const [speed, setSpeed] = useState(0.1);
+    const [map, setMap] = useState("geoOrthographic")
 
     return (
         <div>
-            <h1 className="header">Typescript Demo</h1>
-            <ReactSlider
-                    ariaLabel="globe-speed-slider"
-                    min={-2}
-                    max={2}
-                    defaultValue={0.1}
-                    step={0.1}
-                    renderThumb={(props, state) => <div {...props}> | </div>}
-                    onChange={(state) => setSpeed(state)}
-                    thumbClassName="speed-slider-thumb"
-                    withTracks={true}                
-            />
-            <Speed num={speed} />
+            <h1 className="header">Typescript Demonstration with React Simple Maps</h1>
+                <FiChevronsLeft className="slider-arrow"/>
+                    <ReactSlider
+                            ariaLabel="globe-speed-slider"
+                            min={-2}
+                            max={2}
+                            defaultValue={0.1}
+                            step={0.1}
+                            renderThumb={(props, state) => <div {...props}> {speed} </div>}
+                            onChange={(state) => setSpeed(state)}
+                            thumbClassName="slider-thumb"
+                            withTracks={true}                
+                    />
+                <FiChevronsRight className="slider-arrow"/>
+            <MapConfig map={map} speed={speed} />
             <Map speed={speed} />
         </div>
     );
